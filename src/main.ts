65732b69faa5b076bbe6437cbfd49d359c4e2fda
Manuel Herrera <manuel.herrera@owasp.org>
2022-09-19 21:44:20 -0300

    feat: add nats adapter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.NATS,
+    options: {
+      servers: [process.env.NATS_SERVER],
+      queue: 'authz',
+    },
+  });
+  await app.listen(9990);
 }
 bootstrap();
